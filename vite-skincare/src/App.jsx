44ba90c8032eb92d3e2df88aba1f1f83ebdbffc3
Emmanuel Rojales <ejrojales@gmail.com>
2023-03-25 00:04:27 +1000

    Drag and drop between sample and custom routines

--- vite-skincare/src/App.jsx
+++ vite-skincare/src/App.jsx
@@ -20,31 +20,43 @@ const RoutineStyle = styled.div`
 `;
 
 const PRODUCTS = [
-  { id: 1, step: "Cleanse", name: "Senka Perfect Whip Cleanser" },
-  { id: 2, step: "Cleanse", name: "Cerave Acne Foaming Cleanser" },
-  { id: 3, step: "Moisturize", name: "Aestura AtoBarrier Moisturizer" },
-  { id: 4, step: "Protect", name: "Round Lab Birch Juice Sunscreen" },
+  { id: 0, step: "Cleanse", name: "Senka Perfect Whip Cleanser" },
+  { id: 1, step: "Cleanse", name: "Cerave Acne Foaming Cleanser" },
+  { id: 2, step: "Moisturize", name: "Aestura AtoBarrier Moisturizer" },
+  { id: 3, step: "Protect", name: "Round Lab Birch Juice Sunscreen" },
 ];
 
 
-function RoutineTable({ products, routine }) {
+const CUSTOMPRODUCTS = [];
+
+
+function RoutineTable({ products, customProducts, routine }) {
 
   return (
     <div>
-      <RoutineStep products={products} step="Cleanse" routine={routine} />
-      <RoutineStep products={products} step="Moisturize" routine={routine} />
-      <RoutineStep products={products} step="Protect" routine={routine} />
+      <RoutineStep products={products} customProducts={customProducts} step="Cleanse" routine={routine} />
+      <RoutineStep products={products} customProducts={customProducts} step="Moisturize" routine={routine} />
+      <RoutineStep products={products} customProducts={customProducts} step="Protect" routine={routine} />
     </div>
   );
 }
 
-function RoutineStep({ products, step, routine }) {
+function RoutineStep({ products, customProducts, step, routine }) {
 
   let stepProducts = [];
 
   if (routine === "Sample") {
     // for each product, if product.step == step: append product to stepProducts
     products.forEach((product) => {
+      if (product.step == step) {
+        stepProducts.push(product);
+      };
+    });
+  };
+
+  if (routine === "Custom") {
+    // for each product, if product.step == step: append product to stepProducts
+    customProducts.forEach((product) => {
       if (product.step == step) {
         stepProducts.push(product);
       };
@@ -78,7 +90,6 @@ function RoutineProduct({ products }) {
     <ul>
       {products.map((product, index) => {
         return (
-
           <Draggable key={product.id} draggableId={product.id.toString()} index={index}>
             {(provided) => (
               <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>{product.name}</li>
@@ -93,17 +104,34 @@ function RoutineProduct({ products }) {
 
 function App() {
 
-  const [products, updateProducts] = useState(PRODUCTS)
+  const [products, updateProducts] = useState(PRODUCTS);
+  const [customProducts, updateCustomProducts] = useState(CUSTOMPRODUCTS);
+  const hashmap = {
+    "SampleCleanse": "CustomCleanse",
+    "SampleMoisturize": "CustomMoisturize",
+    "SampleProtect": "CustomProtect"
+  }
 
   function handleOnDragEnd(result) {
 
     if (!result.destination) return;
 
-    const items = Array.from(products); // Create a copy array of products
-    const [reorderedItem] = items.splice(result.source.index, 1); // Remove the item from the copy array
-    items.splice(result.destination.index, 0, reorderedItem); // Add the item to the reordered index
+    console.log(result)
 
-    updateProducts(items); // update the state of products to re-render the newly updated products
+    if (result.destination.droppableId === hashmap[result.source.droppableId]) {
+
+      const sample = Array.from(products); // Create a copy array of sample products
+      const custom = Array.from(customProducts); // Create a copy array of custom products
+
+      let index = sample.findIndex(product => product.id.toString() === result.draggableId)
+      const [reorderedItem] = sample.splice(index, 1); // Remove the item from the sample products
+
+      custom.splice(0, 0, reorderedItem); // Add the item to the customProducts
+
+      updateProducts(sample); // update the state of sample products
+      updateCustomProducts(custom); // update the state of sample products
+    };
+
   }
 
   return (
@@ -114,12 +142,12 @@ function App() {
 
         <RoutineStyle>
           <h2>Sample Routine</h2>
-          <RoutineTable products={products} routine="Sample" />
+          <RoutineTable products={products} customProducts={customProducts} routine="Sample" />
         </RoutineStyle>
 
         <RoutineStyle>
           <h2>Build Your Own Routine</h2>
-          <RoutineTable products={products} routine="Custom" />
+          <RoutineTable products={products} customProducts={customProducts} routine="Custom" />
         </RoutineStyle>
 
       </DragDropContext>
