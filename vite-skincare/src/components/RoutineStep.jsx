2adae54b795d2b8e91f416cb40d2d36908337f89
Emmanuel Rojales <ejrojales@gmail.com>
2023-03-31 11:11:37 +1000

    add product button

--- vite-skincare/src/components/RoutineStep.jsx
+++ vite-skincare/src/components/RoutineStep.jsx
@@ -1,8 +1,15 @@
 import { StrictModeDroppable as Droppable } from "../helpers/StrictModeDroppable";
+import { useRef } from "react";
 import RoutineProduct from "./RoutineProduct";
 
 function RoutineStep({ sampleProducts, customProducts, step, routine }) {
+    const ref = useRef();
+    const handleClick = () => {
+        ref.current.classList.toggle('show-form')
+    }
 
+    // each droppableId needs to be unique
+    let dropId = routine + step;
     let stepProducts = [];
 
     if (routine === "Sample") {
@@ -12,9 +19,22 @@ function RoutineStep({ sampleProducts, customProducts, step, routine }) {
                 sampleProducts[i][routine + step].forEach(product => {
                     stepProducts.push(product)
                 })
-
             }
-        }
+        };
+        return (
+            <>
+                <h2>{step}</h2>
+                <Droppable droppableId={dropId}>
+                    {(provided) => (
+                        <section {...provided.droppableProps} ref={provided.innerRef}>
+                            <RoutineProduct products={stepProducts} />
+                            {provided.placeholder}
+                        </section>
+                    )}
+                </Droppable>
+            </>
+
+        );
     };
 
     if (routine === "Custom") {
@@ -26,26 +46,28 @@ function RoutineStep({ sampleProducts, customProducts, step, routine }) {
                 })
             }
         }
+
+        return (
+            <>
+                <h2>{step}</h2>
+                <Droppable droppableId={dropId}>
+                    {(provided) => (
+                        <section {...provided.droppableProps} ref={provided.innerRef}>
+                            <RoutineProduct products={stepProducts} />
+                            <section className="form">
+                                <li>
+                                    <button className="form-btn" name="name" value="Name" onClick={handleClick}>Add a Product </button>
+                                    <input className="hidden-form" type="text" name="nm" ref={ref}></input>
+                                </li>
+                            </section>
+                            {provided.placeholder}
+                        </section>
+                    )}
+                </Droppable>
+            </>
+
+        );
     };
-
-    // each droppableId needs to be unique
-    let dropId = routine + step;
-    return (
-        <>
-            <h2>{step}</h2>
-            <Droppable droppableId={dropId}>
-
-                {(provided) => (
-                    <section {...provided.droppableProps} ref={provided.innerRef}>
-
-                        <RoutineProduct products={stepProducts} />
-                        {provided.placeholder}
-                    </section>
-                )}
-            </Droppable>
-        </>
-
-    );
 }
 
 export default RoutineStep
