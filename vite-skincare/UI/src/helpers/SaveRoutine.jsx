98e58bfb7e0734237d26a978e1e352d0f80b41a1
Emmanuel Rojales <ejrojales@gmail.com>
2023-07-16 14:48:16 +1000

    Reorganized helper functions into folder

---
+++ vite-skincare/UI/src/helpers/SaveRoutine.jsx
@@ -0,0 +1,57 @@
+const SaveRoutine = async (routineTime, AMPRODUCTS, PMPRODUCTS) => {
+
+    if (routineTime === "am") {
+
+        const newRoutine = {
+            title: "Acne Routine",
+            author: "Emmanuel",
+            comments: [{ "body": "No comment" }],
+            hidden: false,
+            products: {
+                "cleanse": AMPRODUCTS[0]["AmCleanse"],
+                "moisturize": AMPRODUCTS[1]["AmMoisturize"],
+                "protect": AMPRODUCTS[2]["AmProtect"]
+            }
+        };
+        const response = await fetch('/AMroutines', {
+            method: 'POST',
+            body: JSON.stringify(newRoutine),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 201) {
+            alert("Successfully added the routine!");
+        } else {
+            alert(`Failed to add routine, status code = ${response.status}`);
+        }
+
+    } else {
+        const newRoutine = {
+            title: "Acne Routine",
+            author: "Emmanuel",
+            comments: [{ "body": "No comment" }],
+            hidden: false,
+            products: {
+                "cleanse": PMPRODUCTS[0]["PmCleanse"],
+                "moisturize": PMPRODUCTS[1]["PmMoisturize"],
+                "protect": PMPRODUCTS[2]["PmProtect"]
+            }
+        };
+        const response = await fetch('/PMroutines', {
+            method: 'POST',
+            body: JSON.stringify(newRoutine),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 201) {
+            alert("Successfully added the routine!");
+        } else {
+            alert(`Failed to add routine, status code = ${response.status}`);
+        }
+    }
+
+};
+
+export default SaveRoutine
