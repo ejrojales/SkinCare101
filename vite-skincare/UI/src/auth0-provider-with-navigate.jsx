d580ddf3e530e3ab5390bcc387d92c95914a3b87
Emmanuel Rojales <ejrojales@gmail.com>
2023-12-03 18:01:34 +1000

    redirect uri

--- vite-skincare/UI/src/auth0-provider-with-navigate.jsx
+++ vite-skincare/UI/src/auth0-provider-with-navigate.jsx
@@ -7,7 +7,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
     const domain = "dev-o5rxkrrd1uyhy7ej.us.auth0.com";
     const clientId = "nLPCeWdxE1ZxHaZHNsW9dyO5NZKoqcck";
-    const redirectUri = "https://local.d1g59zf0mc35py.amplifyapp.com";
+    const redirectUri = "https://dev.d2lj2rladw06j3.amplifyapp.com/";
     const audience = "https://tmcfzmku2xu5akd5qp2kzphhvm0onvhw.lambda-url.ap-southeast-2.on.aws/"
 
     const onRedirectCallback = (appState) => {
